f70f6d919f21a120c844376dd27bb97e1adc1aad
GoitStudentsWorks <GoitStudentsWorks@users.noreply.github.com>
2024-09-15 21:11:10 +0000

    deploy: db54839cd93723fc9e0fa90b57d775332018ca85

---
+++ assets/selectors-CREpDEMI.js
@@ -0,0 +1,2 @@
+const t=s=>s.transactions.items,a=s=>s.transactions.periodTransactions,n=s=>s.transactions.currentTransaction,c=s=>s.transactions.categories;export{c as a,t as b,a as c,n as s};
+//# sourceMappingURL=selectors-CREpDEMI.js.map
